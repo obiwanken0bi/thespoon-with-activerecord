2491f130333a58327e546e4e018f95e0afe8bbb5
Alexandre Fraschini <alexandre.fraschini@gmail.com>
2020-05-13 20:04:35 +0200

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
+
+  resources :restaurants
 end
